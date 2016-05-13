bf192c57e9d54bc6bb4dd1c97dde7b8031ccddd8
dev_Scott <chengscott@live.com>
2016-05-13 22:21:35 +0800

    clean geometry

--- Computational Geometry/Geometry.cpp
+++ Computational Geometry/Geometry.cpp
@@ -6,45 +6,21 @@ struct point{
 	T x,y;
 	point(){}
 	point(const T&dx,const T&dy):x(dx),y(dy){}
-	inline const point operator+(const point &b)const{
-		return point(x+b.x,y+b.y);
-	}
-	inline const point operator-(const point &b)const{
-		return point(x-b.x,y-b.y);
-	}
-	inline const point operator*(const point &b)const{
-		return point(x*b.x,y*b.y);
-	}
-	inline const point operator/(const point &b)const{
-		return point(x/b.x,y/b.y);
-	}
-	inline const point operator+(const T &b)const{
-		return point(x+b,y+b);
-	}
-	inline const point operator-(const T &b)const{
-		return point(x-b,y-b);
-	}
-	inline const point operator*(const T &b)const{
-		return point(x*b,y*b);
-	}
-	inline const point operator/(const T &b)const{
-		return point(x/b,y/b);
-	}
-	inline bool operator==(const point &b)const{
-		return x==b.x&&y==b.y;
-	}
-	inline const T dot(const point &b)const{
-		return x*b.x+y*b.y;
-	}
-	inline const T cross(const point &b)const{
-		return x*b.y-y*b.x;
-	}
-	inline point normal()const{/*求法向量*/
-		return point(-y,x);
-	}
-	inline const T abs2()const{/*向量長度的平方*/
-		return dot(*this);
-	}
+	point operator+(const point &b)const{return point(x+b.x,y+b.y);}
+	point operator-(const point &b)const{return point(x-b.x,y-b.y);}
+	point operator*(const point &b)const{return point(x*b.x,y*b.y);}
+	point operator/(const point &b)const{return point(x/b.x,y/b.y);}
+	point operator+(const T &b)const{return point(x+b,y+b);}
+	point operator-(const T &b)const{return point(x-b,y-b);}
+	point operator*(const T &b)const{return point(x*b,y*b);}
+	point operator/(const T &b)const{return point(x/b,y/b);}
+	bool operator==(const point &b)const{return x==b.x&&y==b.y;}
+	T dot(const point &b)const{return x*b.x+y*b.y;}
+	T cross(const point &b)const{return x*b.y-y*b.x;}
+	// 法向量
+	point normal()const{return point(-y,x);}
+	// 向量長度的平方
+	T abs2()const{return dot(*this);}
 };
 template<typename T>
 struct line{
@@ -52,18 +28,17 @@ struct line{
 	point<T> p1,p2;
 	T a,b,c;/*ax+by+c=0*/
 	line(const point<T>&x,const point<T>&y):p1(x),p2(y){}
-	inline void pton(){/*轉成一般式*/ 
+	void pton(){/*轉成一般式*/ 
 		a=p1.y-p2.y;
 		b=p2.x-p1.x;
 		c=-a*p1.x-b*p1.y;
 	}
-	inline T cross(const point<T> &p)const{/*點和有向直線的關係，>0左邊、=0在線上<0右邊*/
-		return (p2-p1).cross(p-p1);
-	}
-	inline bool point_on_segment(const point<T>&p)const{/*點是否線段上*/ 
+	/*點和有向直線的關係，>0左邊、=0在線上<0右邊*/
+	T cross(const point<T> &p)const{return (p2-p1).cross(p-p1);}
+	bool point_on_segment(const point<T>&p)const{/*點是否線段上*/ 
 		return cross(p)==0&&(p1-p).dot(p2-p)<=0; 
 	}
-	inline T dis2(const point<T> &p,bool is_segment=0)const{/*點跟直線/線段的距離平方*/ 
+	T dis2(const point<T> &p,bool is_segment=0)const{/*點跟直線/線段的距離平方*/ 
 		point<T> v=p2-p1,v1=p-p1;
 		if(is_segment){
 			point<T> v2=p-p2;
@@ -73,7 +48,7 @@ struct line{
 		T tmp=v.cross(v1);
 		return tmp*tmp/v.abs2();
 	}
-	inline point<T> mirror(const point<T> &p)const{/*點對直線的鏡射*/ 
+	point<T> mirror(const point<T> &p)const{/*點對直線的鏡射*/ 
 		/*要先呼叫pton轉成一般式*/
 		point<T> ans;
 		T d=a*a+b*b;
@@ -81,19 +56,17 @@ struct line{
 		ans.y=(a*a*p.y-b*b*p.y-2*a*b*p.x-2*b*c)/d;
 		return ans;
 	}
-	inline bool equal(const line &l)const{/*直線相等*/ 
-		return cross(l.p1)==0&&cross(l.p2)==0;
-	}
-	inline bool parallel(const line &l)const{/*直線平行*/ 
-		return (p1-p2).cross(l.p1-l.p2)==0;
-	}
-	inline bool cross_seg(const line &l)const{/*直線是否交線段*/
+	/*直線相等*/ 
+	bool equal(const line &l)const{return cross(l.p1)==0&&cross(l.p2)==0;}
+	/*直線平行*/
+	bool parallel(const line &l)const{return (p1-p2).cross(l.p1-l.p2)==0;}
+	bool cross_seg(const line &l)const{/*直線是否交線段*/
 		return (p2-p1).cross(l.p1)*(p2-p1).cross(l.p2)<=0;
 	}
-	inline char line_intersect(const line &l)const{/*直線相交情況，-1無限多點、1交於一點、0不相交*/ 
-		return parallel(l)?(cross(l.p1)==0?-1:0):1;
-	}
-	inline char seg_intersect(const line &l)const{/*線段相交情況，-1無限多點、1交於一點、0不相交*/ 
+	/*直線相交情況，-1無限多點、1交於一點、0不相交*/
+	char line_intersect(const line &l)const{return parallel(l)?(cross(l.p1)==0?-1:0):1;}
+	/*線段相交情況，-1無限多點、1交於一點、0不相交*/
+	char seg_intersect(const line &l)const{
 		T c1=(p2-p1).cross(l.p1-p1);
 		T c2=(p2-p1).cross(l.p2-p1);
 		T c3=(l.p2-l.p1).cross(p1-l.p1);
@@ -112,12 +85,12 @@ struct line{
 		if(c4==0&&l.point_on_segment(p2))return 1;
 		return 0;
 	}
-	inline point<T> line_intersection(const line &l)const{/*直線交點*/ 
+	point<T> line_intersection(const line &l)const{/*直線交點*/ 
 		point<T> a=p2-p1,b=l.p2-l.p1,s=l.p1-p1;
 		//if(a.cross(b)==0)return INF;
 		return p1+a*s.cross(b)/a.cross(b);
 	}
-	inline point<T> seg_intersection(const line &l)const{/*線段交點*/ 
+	point<T> seg_intersection(const line &l)const{/*線段交點*/ 
 		T c1=(p2-p1).cross(l.p1-p1);
 		T c2=(p2-p1).cross(l.p2-p1);
 		T c3=(l.p2-l.p1).cross(p1-l.p1);
@@ -141,18 +114,18 @@ template<typename T>
 struct polygon{
 	polygon(){}
 	std::vector<point<T> > p;
-	inline const point<T>& operator[](int id)const{
-		return p[id];
-	}
-	inline T area()const{/*多邊形面積*/ 
+	const point<T>& operator[](int id)const{return p[id];}
+	T area()const{/*多邊形面積*/ 
 		T ans=0;
-		for(int i=p.size()-1,j=0;j<(int)p.size();i=j++)
+		size_t psize=p.size();
+		for(size_t i= psize-1,j=0;j<psize;i=j++)
 			ans+=p[i].cross(p[j]);
 		return ans/2;
 	}
-	inline point<T> gravity()const{/*多邊形重心*/
+	point<T> center_of_mass()const{/*多邊形重心*/
 		T cx=0,cy=0,w=0;
-		for(int i=p.size()-1,j=0;j<(int)p.size();i=j++){
+		size_t psize=p.size();
+		for(size_t i=psize-1,j=0;j<psize;i=j++){
 			T a=p[i].cross(p[j]);
 			cx+=(p[i].x+p[j].x)*a;
 			cy+=(p[i].y+p[j].y)*a;
@@ -160,25 +133,29 @@ struct polygon{
 		}
 		return point<T>(cx/3/w,cy/3/w);
 	}
-	inline char chas(const point<T> &x)const{/*點是否在凸多邊形內，是的話回傳1、在邊上回傳-1、否則回傳0*/
+	/*點是否在凸多邊形內，是的話回傳1、在邊上回傳-1、否則回傳0*/
+	char chas(const point<T> &x)const{
 		T tp=0,np;
-		for(int i=p.size()-1,j=0;j<(int)p.size();i=j++){
+		size_t psize = p.size();
+		for(size_t i=psize-1,j=0;j<psize;i=j++){
 			if(!(np=(p[j]-x).cross(p[i]-x)))return -1;
 			if(tp*np<0)return 0;
 			tp=(np!=0)?np:tp;
 		}
 		return 1;
 	}
-	inline char ahas(const point<T>& t)const{/*點是否在簡單多邊形內，是的話回傳1、在邊上回傳-1、否則回傳0*/ 
+	/*點是否在簡單多邊形內，是的話回傳1、在邊上回傳-1、否則回傳0*/
+	char ahas(const point<T>& t)const{
 		bool c=0;
-		for(int i=0,j=p.size()-1;i<p.size();j=i++)
+		size_t psize = p.size();
+		for(size_t i=0,j=psize-1;i<psize;j=i++)
 			if(line<T>(p[i],p[j]).point_on_segment(t))return -1;
 			else if((p[i].y>t.y)!=(p[j].y>t.y)&&
 			t.x<(p[j].x-p[i].x)*(t.y-p[i].y)/(p[j].y-p[i].y)+p[i].x)
 				c=!c;
 		return c;
 	}
-	inline polygon cut(const line<T> &l)const{/*凸包對直線切割，得到直線l左側的凸包*/
+	polygon cut(const line<T> &l)const{/*凸包對直線切割，得到直線l左側的凸包*/
 		polygon ans;
 		for(int n=p.size(),i=n-1,j=0;j<n;i=j++){
 			if(l.cross(p[i])>=0){
@@ -190,14 +167,14 @@ struct polygon{
 		}
 		return ans;
 	}
-	inline static bool graham_cmp(const point<T>& a,const point<T>& b){
+	static bool graham_cmp(const point<T>& a,const point<T>& b){
 		return (a.x<b.x)||(a.x==b.x&&a.y<b.y);/*凸包排序函數*/
 	}
-	inline void graham(std::vector<point<T> > &s){/*凸包*/
+	void graham(std::vector<point<T> > &s){/*凸包*/
 		sort(s.begin(),s.end(),graham_cmp);
 		p.resize(s.size()+1);
 		int m=0;
-		for(int i=0;i<(int)s.size();++i){
+		for(size_t i=0;i<s.size();++i){
 			while(m>=2&&(p[m-1]-p[m-2]).cross(s[i]-p[m-2])<=0)--m;
 			p[m++]=s[i];
 		}
@@ -208,16 +185,14 @@ struct polygon{
 		if(s.size()>1)--m; 
 		p.resize(m);
 	}
-	inline static char sign(const T&x){
-		return x>=0?1:-1;
-	}
-	inline static bool angle_cmp(const line<T>& A,const line<T>& B){
+	static char sign(const T&x){return x>=0?1:-1;}
+	static bool angle_cmp(const line<T>& A,const line<T>& B){
 		point<T>a=A.p2-A.p1,b=B.p2-B.p1;
 		//return atan2(a.y,a.x)<atan2(b.y,b.x); 
 		char ay=sign(a.y),by=sign(b.y),ax=sign(a.x),bx=sign(b.x);
 		return ay>by||(ay==by&&(ax*ay>bx*by||(ax*ay==bx*by&&a.cross(b)>0)));
 	}
-	inline int halfplane_intersection(std::vector<line<T> > &s){
+	int halfplane_intersection(std::vector<line<T> > &s){
 		sort(s.begin(),s.end(),angle_cmp);
 		int L,R,n=s.size();
 		std::vector<point<T> > px(n);
@@ -246,30 +221,24 @@ struct triangle{
 	point<T> a,b,c;
 	triangle(){}
 	triangle(const point<T> &a,const point<T> &b,const point<T> &c):a(a),b(b),c(c){}
-	inline T area()const{
+	T area()const{
 		T t=(b-a).cross(c-a)/2;
 		return t>0?t:-t;
 	}
-	inline point<T> barycenter()const{/*重心*/
-		return (a+b+c)/3;
-	}
-	inline point<T> circumcenter()const{/*外心*/
+	/*重心*/
+	point<T> center_of_mass()const{return (a+b+c)/3;}
+	point<T> circumcenter()const{/*外心*/
 		static line<T> u,v;
-		u.p1.x=(a.x+b.x)/2;
-		u.p1.y=(a.y+b.y)/2;
-		u.p2.x=u.p1.x-a.y+b.y;
-		u.p2.y=u.p1.y+a.x-b.x;
-		v.p1.x=(a.x+c.x)/2;
-		v.p1.y=(a.y+c.y)/2;
-		v.p2.x=v.p1.x-a.y+c.y;
-		v.p2.y=v.p1.y+a.x-c.x;
+		u.p1=(a+b)/2;
+		u.p2.x=u.p1.x-a.y+b.y,u.p2.y=u.p1.y+a.x-b.x;
+		v.p1=(a+c)/2;
+		v.p2.x=v.p1.x-a.y+c.y,v.p2.y=v.p1.y+a.x-c.x;
 		return u.line_intersection(v);
 	}
-	inline point<T> incenter()const{/*內心，用到根號*/
+	point<T> incenter()const{/*內心，用到根號*/
 		T A=sqrt((b-c).abs2()),B=sqrt((a-c).abs2()),C=sqrt((a-b).abs2());
 		return point<T>(A*a.x+B*b.x+C*c.x,A*a.y+B*b.y+C*c.y)/(A+B+C);
 	}
-	inline point<T> perpencenter()const{/*垂心*/
-		return barycenter()*3-circumcenter()*2;
-	}
+	/*垂心*/
+	point<T> perpencenter()const{return center_of_mass()*3-circumcenter()*2;}
 };
