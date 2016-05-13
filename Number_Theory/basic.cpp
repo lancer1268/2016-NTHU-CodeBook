0a70c21bff8b6e135cac3204807015d380c10457
LFsWang <tnst92002@gmail.com>
2016-05-13 22:40:36 +0800

    iscom=>isp

--- Number_Theory/basic.cpp
+++ Number_Theory/basic.cpp
@@ -6,15 +6,15 @@ void gcd(const T &a,const T &b,T &d,T &x,T &y){
 }
 
 const int MAXPRIME = 1000000;
-int iscom[MAXPRIME], prime[MAXPRIME], primecnt;
+int isp[MAXPRIME], prime[MAXPRIME], primecnt;
 void sieve() {
-    memset(iscom,0,sizeof(iscom));
+    memset(isp,0,sizeof(isp));
     primecnt = 0;
     for(int i=2;i<MAXPRIME;++i) {
-        if(!iscom[i]) prime[primecnt++] = i;
+        if(!isp[i]) prime[primecnt++] = i;
         for(int j=0;j<primecnt;++j) {
             if(i*prime[j]>=MAXPRIME) break;
-            iscom[i*prime[j]] = prime[j];
+            isp[i*prime[j]] = prime[j];
             if(i%prime[j]==0) break;
         }
     }
