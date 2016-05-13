f44783de86d52365c63de85425f33d4cbb08fd2c
dev_Scott <chengscott@live.com>
2016-05-13 22:41:49 +0800

    clean flow

--- Flow/SAP.cpp
+++ Flow/SAP.cpp
@@ -1,29 +1,29 @@
 #include<string.h>
 #include<algorithm>
-#define MAXN 100
+#define MAXN 100+5
 int n,m;/*點數、邊數*/
-int g[MAXN+5][MAXN+5],f[MAXN+5][MAXN+5],r[MAXN+5][MAXN+5];
 /*容量上限、流量、剩餘容量*/ 
-int d[MAXN+5],gap[MAXN+5];
+int g[MAXN][MAXN],f[MAXN][MAXN],r[MAXN][MAXN];
 /*層次、gap[i]=層次為i的點之個數*/ 
+int d[MAXN],gap[MAXN];
 int dfs(int x,int flow,int s,int t){
 	if(x==t)return flow;
-	int tmd=flow,tmp;
+	int rf=flow,tmp;
 	for(int i=1;i<=n;++i){
 		if(r[x][i]&&d[x]==d[i]+1){
-			tmp=dfs(i,std::min(tmd,r[x][i]),s,t);
+			tmp=dfs(i,std::min(rf,r[x][i]),s,t);
 			f[x][i]+=tmp;
 			f[i][x]-=tmp;
 			r[x][i]=g[x][i]-f[x][i];
 			r[i][x]=g[i][x]-f[i][x];
-			if(!(tmd-=tmp))return flow;
+			if(!(rf-=tmp))return flow;
 		}
 	}
 	if(!--gap[d[x]])d[s]=n;
 	++gap[++d[x]];
-	return flow-tmd;
+	return flow-rf;
 }
-inline int sap(int s,int t){
+int sap(int s,int t){
 	memset(f,0,sizeof(f));
 	memset(d,0,sizeof(d));
 	memset(gap,0,sizeof(gap));
--- Flow/dinic.cpp
+++ Flow/dinic.cpp
@@ -1,19 +1,19 @@
 #include<string.h>
 #include<queue>
 #include<algorithm>
-#define MAXN 100
+#define MAXN 100+5
 int n,m;/*點數、邊數*/
-int g[MAXN+5][MAXN+5],f[MAXN+5][MAXN+5],r[MAXN+5][MAXN+5];
 /*容量上限、流量、剩餘容量*/ 
-int level[MAXN+5];/*層次*/ 
-inline int bfs(int s,int t){
+int g[MAXN][MAXN],f[MAXN][MAXN],r[MAXN][MAXN];
+int level[MAXN];/*層次*/ 
+int bfs(int s,int t){
 	memset(level,0,sizeof(level));
-	std::queue<int >q;
+	std::queue<int>q;
 	q.push(s);
 	level[s]=1;
-	while(q.size()){
+	while(!q.empty()){
 		int x=q.front();q.pop();
-		for(int i=1;i<=n;i++){
+		for(int i=1;i<=n;++i){
 			if(!level[i]&&r[x][i]){
 				level[i]=level[x]+1;
 				q.push(i);
@@ -24,25 +24,25 @@ inline int bfs(int s,int t){
 }
 int dfs(int x,int flow,int t){
 	if(x==t)return flow;
-	int tmd=flow,tmp;
-	for(int i=1;i<=n&&tmd;++i){
+	int rf=flow,tmp;
+	for(int i=1;i<=n&&rf;++i){
 		if(level[i]==level[x]+1&&r[x][i]){
-			tmp=dfs(i,std::min(tmd,r[x][i]),t);
+			tmp=dfs(i,std::min(rf,r[x][i]),t);
 			f[x][i]+=tmp;
 			f[i][x]-=tmp;
 			r[x][i]=g[x][i]-f[x][i];
 			r[i][x]=g[i][x]-f[i][x];
-			if(!(tmd-=tmp))return flow;
+			if(!(rf-=tmp))return flow;
 		}
 	}
-	return flow-tmd;
+	return flow-rf;
 }
-inline int dinic(int s,int t){
+int dinic(int s,int t){
 	memset(f,0,sizeof(f));
 	memcpy(r,g,sizeof(g));
-	int ans=0,tmd=0;
+	int ret=0,f=0;
 	while(bfs(s,t)){
-		while((tmd=dfs(s,INT_MAX,t)))ans+=tmd;
+		while((f=dfs(s,INT_MAX,t)))ret+=f;
 	}
-	return ans;
+	return ret;
 }
