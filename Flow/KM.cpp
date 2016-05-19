eeefbcfd831a61da1e5b5ccf9478247ba966923d
jacky860226 <jacky860226@yahoo.com.tw>
2016-05-19 16:36:53 +0800

    KM O(N^3)版本

--- Flow/KM.cpp
+++ Flow/KM.cpp
@@ -1,61 +1,64 @@
-struct KM{
-// Maximum Bipartite Weighted Matching (Perfect Match)
-	static const int MXN = 650;
-	static const int INF = 2147483647; // long long
-	int n,match[MXN],vx[MXN],vy[MXN];
-	int edge[MXN][MXN],lx[MXN],ly[MXN],slack[MXN];
-	// ^^^^ long long
-	void init(int _n){
-		n = _n;
-		for (int i=0; i<n; i++)
-			for (int j=0; j<n; j++)
-				edge[i][j] = 0;
+#define MAXN 100
+int n;
+int g[MAXN][MAXN],lx[MAXN],ly[MAXN],slack_y[MAXN];
+int match_y[MAXN];
+bool vx[MAXN],vy[MAXN];//要保證g是完全二分圖
+bool dfs(int x,bool adjust=1){//DFS找增廣路，is=1表示要交換邊 
+	if(vx[x])return 0;
+	vx[x]=1;
+	for(int y=0;y<n;++y){
+		if(vy[y])continue;
+		int t=lx[x]+ly[y]-g[x][y];
+		if(t==0){
+			vy[y]=1;
+			if(match_y[y]==-1||dfs(match_y[y],adjust)){
+				if(adjust)match_y[y]=x;
+				return 1;
+			}
+		}else if(slack_y[y]>t)slack_y[y]=t;
 	}
-	void add_edge(int x, int y, int w){ // long long
-		edge[x][y] = w;
+	return 0;
+}
+inline int km(){
+	memset(ly,0,sizeof(int)*n);
+	memset(match_y,-1,sizeof(int)*n);
+	for(int x=0;x<n;++x){
+		lx[x]=0;
+		for(int y=0;y<n;++y){
+			lx[x]=max(lx[x],g[x][y]);
+		}
 	}
-	bool DFS(int x){
-		vx[x] = 1;
-		for (int y=0; y<n; y++){
-			if (vy[y]) continue;
-			if (lx[x]+ly[y] > edge[x][y]){
-				slack[y] = min(slack[y], lx[x]+ly[y]-edge[x][y]);
-			} else {
-				vy[y] = 1;
-				if (match[y] == -1 || DFS(match[y])){
-					match[y] = x;
-					return true;
+	for(int x=0;x<n;++x){
+		for(int y=0;y<n;++y)slack_y[y]=INT_MAX;
+		memset(vx,0,sizeof(bool)*n);
+		memset(vy,0,sizeof(bool)*n);
+		if(dfs(x))continue;
+		bool flag=1;
+		while(flag){
+			int cut=INT_MAX;
+			for(int y=0;y<n;++y){
+				if(!vy[y]&&cut>slack_y[y])cut=slack_y[y];
+			}
+			for(int j=0;j<n;++j){
+				if(vx[j])lx[j]-=cut;
+				if(vy[j])ly[j]+=cut;
+				else slack_y[j]-=cut;
+			}
+			for(int y=0;y<n;++y){
+				if(!vy[y]&&slack_y[y]==0){
+					vy[y]=1;
+					if(match_y[y]==-1||dfs(match_y[y],0)){
+						flag=0;//測試成功，有增廣路 
+						break;
+					}
 				}
 			}
 		}
-		return false;
+		memset(vx,0,sizeof(bool)*n);
+		memset(vy,0,sizeof(bool)*n);
+		dfs(x);//最後要記得將邊翻反轉 
 	}
-	int solve(){
-		fill(match,match+n,-1);
-		fill(lx,lx+n,-INF);
-		fill(ly,ly+n,0);
-		for (int i=0; i<n; i++)
-			for (int j=0; j<n; j++)
-				lx[i] = max(lx[i], edge[i][j]);
-		for (int i=0; i<n; i++){
-			fill(slack,slack+n,INF);
-			while (true){
-				fill(vx,vx+n,0);
-				fill(vy,vy+n,0);
-				if ( DFS(i) ) break;
-				int d = INF; // long long
-				for (int j=0; j<n; j++)
-					if (!vy[j]) d = min(d, slack[j]);
-				for (int j=0; j<n; j++){
-					if (vx[j]) lx[j] -= d;
-					if (vy[j]) ly[j] += d;
-					else slack[j] -= d;
-				}
-			}
-		}
-		int res=0;
-		for (int i=0; i<n; i++)
-			res += edge[match[i]][i];
-		return res;
-	}
-}graph;
+	int ans=0;
+	for(int y=0;y<n;++y)ans+=g[match_y[y]][y];
+	return ans;
+}
